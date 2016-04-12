e9a354bc7c34a6afd0e25489f4173b05020aae01
Calum Harris <harris.calum@gmail.com>
2016-04-12 13:49:43 +0100

    Save
    Added UI textField functionality.

--- Meme-Me V1/PickAndEditViewController.swift
+++ Meme-Me V1/PickAndEditViewController.swift
@@ -9,11 +9,74 @@
 import UIKit
 
 class PickAndEditViewController: UIViewController {
+  
+  // MARK: Properties and view cycles
 
+  @IBOutlet weak var cameraOutlet: UIBarButtonItem!
+  @IBOutlet weak var chooseFromAlbumOutlet: UIBarButtonItem!
+  @IBOutlet weak var shareButtonOutlet: UIBarButtonItem!
+  @IBOutlet weak var cancelButtonOutlet: UIBarButtonItem!
+  
+  @IBOutlet weak var topTextField: UITextField!
+  @IBOutlet weak var bottomTextField: UITextField!
+  @IBOutlet weak var imageViewOutlet: UIImageView!
+  
+  let textFieldAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor(), NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 27.0)!, NSStrokeWidthAttributeName : 3.0, NSStrokeColorAttributeName : UIColor.blackColor()]
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    congifureTextFields()
+
   }
 
-
 }
+
+
+
+extension PickAndEditViewController: UITextFieldDelegate {
+  
+  // MARK: textField configuration
+  
+  func congifureTextFields() {
+    
+    topTextField.delegate = self
+    topTextField.defaultTextAttributes = textFieldAttributes
+    let topText = NSAttributedString(string: "Top", attributes: textFieldAttributes)
+    topTextField.attributedPlaceholder = topText
+    topTextField.textAlignment = .Center
+    
+    bottomTextField.delegate = self
+    bottomTextField.textAlignment = .Center
+    let bottomText = NSAttributedString(string: "Bottom", attributes: textFieldAttributes)
+    bottomTextField.attributedPlaceholder = bottomText
+
+    
+    bottomTextField.defaultTextAttributes = textFieldAttributes
+    bottomTextField.textAlignment = .Center
+    
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    topTextField.resignFirstResponder()
+    bottomTextField.resignFirstResponder()
+    return true
+  }
+
+  func textFieldDidBeginEditing(textField: UITextField) {
+    topTextField.attributedPlaceholder = nil
+    bottomTextField.attributedPlaceholder = nil
+  }
+  
+  func textFieldDidEndEditing(textField: UITextField) {
+    congifureTextFields()
+  }
+  
+  
+}
+
+
+
+
 
